9c56cf80c152d1be62353ea56c3af3aef2a6c76f
joshggn32 <joshggn32@gmail.com>
2025-04-02 13:13:37 +0200

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,8 +37,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            addedTokens: 1000000000,
+                            addedXp: 30000000
                         }, e.secret)
                     });
                     fetch("https://api.blooket.com/api/users/add-rewards", {
